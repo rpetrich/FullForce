58be087b366b877644e03dcbadf31ff1a3fc3a18
Ryan Petrich <rpetrich@gmail.com>
2010-05-06 05:24:19 -0600

    Actually do something

--- FullForce.m
+++ FullForce.m
@@ -1,2 +1,20 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <SpringBoard/SpringBoard.h>
+#import <CaptainHook/CaptainHook.h>
+
+CHDeclareClass(SBApplication);
+
+CHDeclareMethod(0, BOOL, SBApplication, isClassic)
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/com.booleanmagic.fullforce.plist"];
+	BOOL value = [[dict objectForKey:[@"FFEnabled-" stringByAppendingString:[self displayIdentifier]]] boolValue];
+	[pool drain];
+	if (value) {
+		CHSuper(0, SBApplication, isClassic);
+		return NO;
+	} else {
+		return CHSuper(0, SBApplication, isClassic);
+	}
+}
