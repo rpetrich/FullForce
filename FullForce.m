699afaac61f2282be73e53458135a4c038a50b2c
Ryan Petrich <rpetrich@gmail.com>
2010-05-26 19:41:21 -0600

    Ignore applications that have the no-fullforce app tag

--- FullForce.m
+++ FullForce.m
@@ -106,6 +106,7 @@ CHMethod(0, BOOL, SBApplication, isClassic)
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/com.booleanmagic.fullforce.plist"];
 	BOOL value = [[dict objectForKey:[@"FFEnabled-" stringByAppendingString:[self displayIdentifier]]] boolValue];
+	value &= ![[self tags] containsObject:@"no-fullforce"];
 	[pool drain];
 	if (value) {
 		CHSuper(0, SBApplication, isClassic);
